91249c7f6cfad02b27d4d0c4c2a0ed1b1056def2
Piero <piero.neri@hotmail.fr>
2025-02-01 04:14:55 +0100

    fix img next

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Image from "next/image";
 
 type Projet = {
     id: number;
@@ -8,15 +9,17 @@ type Projet = {
 };
 
 export default async function Home() {
-    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/projets?populate=*`);
+    const urlApi = process.env.NEXT_PUBLIC_API_URL;
+    const res = await axios.get(`${urlApi}/api/projets?populate=*`);
     const projets: Projet[] = res.data.data;
     return (
         <div>
             <h1 className="text-4xl font-bold">Mon Portfolio</h1>
             {projets.map((projet) => (
+
                 <div key={projet.id}>
                     <h2 className="text-2xl">{projet.title}</h2>
-                    <img src={`${process.env.NEXT_PUBLIC_API_URL}${projet.preview.url}`} alt={projet.title} />
+                    <Image src={urlApi + projet.preview.url} width={800} height={400} alt={projet.title} priority={true} />
                 </div>
             ))}
         </div>
